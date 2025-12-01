d84203cf89a190425b5d6f4c2fe50e2dcb212936
Amina0506 <amina010506@gmail.com>
2025-12-01 15:03:09 +0100

    Css voor loginpagina

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,6 +8,22 @@
             <li><a href="{{ url('/faq') }}">FAQ</a></li>
             <li><a href="{{ url('/contact') }}">Contact</a></li>
             <li>Profiel</li>
+
+            <!--voor niet ingelogde gebruiker-->
+            @guest
+                <li><a href="{{ route('login') }}">Login</a></li>
+            @endguest
+
+            <!--voor ingelogde gebruikers-->
+            @auth
+                <li><a href="{{ url('/profile') }}">Profiel</a></li>
+                <li>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                </li>
+            @endauth
         </ul>
     </nav>
 </header>
