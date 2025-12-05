37a1532fd9c2bee8d177cfb99cc941fdfc1e6010
Amina0506 <amina010506@gmail.com>
2025-12-05 21:20:32 +0100

    Controller en formulier voor contactpagina toegevoegd

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMail;
+
+class ContactController extends Controller
+{
+    public function show(){
+        return view('contact');
+    }
+
+    public function store(Request $request){
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string|max:255',
+        ]);
+
+        Contact::create($request->all());
+
+        Mail::to('admin@example.com')->send(new ContactMail($data));
+
+
+        return back()->with('submitted', true);
+
+    }
+}
+
